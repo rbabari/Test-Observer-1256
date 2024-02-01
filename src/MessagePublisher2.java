0963b75cb46bbed05b1dbc49ba154e4f975afc3b
HelixQc <156359638+HelixQc@users.noreply.github.com>
2024-02-01 15:12:10 -0500

    Update MessagePublisher2.java
    new publissher

--- src/MessagePublisher2.java
+++ src/MessagePublisher2.java
@@ -1,18 +1,26 @@
 public class MessagePublisher2 implements IObservable{
+  
+    List<IObserver> observersList = new ArrayList<>();
     @Override
     public void ajoute(IObserver o) {
-
+        observersList.add(o);
     }
+
     @Override
     public void supprimer(IObserver o) {
-
+        observersList.remove(o);
     }
+
     @Override
-    public void NotifierTout(Message msg1) {
-
+    public void NotifierTout(Message msg) {
+        for(IObserver o : observersList)
+        {
+            o.update(msg);
+        }
     }
+
     @Override
     public int CompterObservers() {
-        return 0;
+        return observersList.size();
     }
 }
