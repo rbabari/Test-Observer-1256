d694175d9ae35f1a7c106b485739f2a0f6329ac7
Fella-Dahmani <f.dahmani.dj@outlook.fr>
2024-02-01 15:21:23 -0500

    Adding some functions & mermaid.md

--- src/MessagePublisher2.java
+++ src/MessagePublisher2.java
@@ -1,18 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessagePublisher2 implements IObservable{
+    List<IObserver> observers = new ArrayList<>();
+
     @Override
     public void ajoute(IObserver o) {
+        observers.add(o);
 
     }
     @Override
     public void supprimer(IObserver o) {
+        observers.remove(o);
 
     }
     @Override
     public void NotifierTout(Message msg1) {
+        for (IObserver iob : observers){
+            iob.update(msg1);
+        }
 
     }
     @Override
     public int CompterObservers() {
-        return 0;
+
+        return observers.size();
     }
 }
